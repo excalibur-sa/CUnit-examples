cbb584eaf8eeea7baabcd3e87f5c30ca10a2e290
Eison <appcasinee@gmail.com>
2016-09-07 14:20:40 +0800

    add Makefile for each example to replace the compile.sh

--- example 2/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-#gcc -o test -I/home/shena/opensrc_install_directory/include -L/home/shena/opensrc_install_directory/lib -lcunit run_test.c test_func.c func.c
-gcc -o test  run_test.c test_func.c func.c -g `pkg-config --cflags --libs cunit`
